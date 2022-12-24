1162d94376c14b26f63b0889fd89964e8abadf81
Vitor Franca <108237482+vitorfrancca@users.noreply.github.com>
2022-12-24 15:54:52 +0100

    Update Origem.cpp

--- tcp_client/Origem.cpp
+++ tcp_client/Origem.cpp
@@ -1,67 +1,32 @@
 #include <iostream>
-#define ASIO_STANDALONE
-#ifdef _WIN32
-#define _WIN32_WINNT 0x0A00
-#endif 
-#include <asio.hpp>
-#include <asio/ts/buffer.hpp>
-#include <asio/ts/internet.hpp>
+#include <boost/asio.hpp>
 
-int main() {
-	asio::error_code ec; //pega erros especificos
+int main()
+{
+    // Obtém as informações de conexão do servidor de achievements
+    std::string ip_address = "127.0.0.1"; // Endereço IP do servidor de achievements
+    unsigned short port = 8080; // Porta do servidor de achievements
 
-	//Cria o "context" 
-	asio::io_context context;
+    // Cria o objeto do servidor
+    boost::asio::ip::tcp::endpoint endpoint(
+        boost::asio::ip::address::from_string(ip_address),
+        port
+    );
 
-	//Pega o endereço no qual estamos conectando1
-	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("192.168.1.100", ec), 1000);
+    // Cria o objeto do socket
+    boost::asio::ip::tcp::socket socket(endpoint.get_io_service());
 
-	//Cria o socket, o context ira entregar a implementaçao
-	asio::ip::tcp::socket socket(context);
+    // Conecta ao servidor de achievements
+    socket.connect(endpoint);
 
-	//Diz ao socket para tentar se conectar
-	socket.connect(endpoint, ec);
+    std::cout << "Conectado ao servidor de achievements!" << std::endl;
 
-	if (!ec) {
-		std::cout << "Conectado!" << std::endl;
-	}
+    // Envia um pacote para o servidor de achievements
+    std::string mensagem = "Olá";
+    boost::asio::write(socket, boost::asio::buffer(mensagem));
 
-	else {
-		std::cout << "Falha de conecxao com o endereço: \n" << ec.message()/*pega mensagem associada ao erro */ << std::endl;
-	}
+    // Executa as operações pendentes no objeto io_service
+    endpoint.get_io_service().run();
 
-	//Verifia se esta conectado
-	if (socket.is_open()) {
-
-		char packet_bytes[] = {"Teles"
-		};
-
-
-
-
-		//Se conexçao for True, escreve:
-		//asio::buffer e um container, contendo bytes da string e sizer.
-		socket.write_some(asio::buffer(packet_bytes, sizeof(packet_bytes)), ec);
-
-		//Estava dando erro pelo tempo de resposta, programa indo mais rapido que a resposta.		
-		socket.wait(socket.wait_read);
-
-		size_t bytes = socket.available();
-		std::cout << "Bytes Available: " << bytes << std::endl;
-
-		if (bytes > 0) {
-			//adiciona dados recebidos em um vector
-			std::vector<char> vBuffer(bytes);
-			socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
-			// 
-			for (auto c : vBuffer) {
-				std::cout << c;
-			}
-		}
-
-
-	}
-	system("pause");
-	return 0;
-
+    return 0;
 }
